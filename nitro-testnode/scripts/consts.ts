c55e4aa86fe16d2d98a2261c3e236ea0c84b7c01
Shubham Tiwari <shubht3303@gmail.com>
2025-05-26 18:18:13 +0530

    yesh

--- nitro-testnode/scripts/consts.ts
+++ nitro-testnode/scripts/consts.ts
@@ -1,5 +1,5 @@
 export const l1keystore = "/home/user/l1keystore";
-export const l1passphrase = "qoneqt-production-passphrase";
+export const l1passphrase = "passphrase";
 export const configpath = "/config";
 export const tokenbridgedatapath = "/tokenbridge-data";
 // Not secure. Do not use for production purposes
